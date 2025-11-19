ae964a2a6e5b7d05c251eeed8b837b7c307b0f11
ericzaim <eric.zaima@gmail.com>
2025-11-19 12:19:17 -0300

    feat: cadastrar users

---
+++ frontend/scripts/cadastrar.js
@@ -0,0 +1,24 @@
+document.getElementById('form-cadastro').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    console.log({ nome, email, senha });
+
+    const response = await fetch('https://pi-2sem.onrender.com/api/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: { nome, email, senha }
+    });
+    
+    if (response.ok) {
+        alert('Usuário cadastrado com sucesso!');
+        document.getElementById('form-cadastro').reset();
+    } else {
+        alert('Erro ao cadastrar usuário. Tente novamente.');
+    }
+});
