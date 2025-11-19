061e9c394ccf0f3cb086e45d58b46390d27ef837
ericzaim <eric.zaima@gmail.com>
2025-11-19 12:37:38 -0300

    fix: nome de campo json errado

--- frontend/scripts/cadastrar.js
+++ frontend/scripts/cadastrar.js
@@ -3,14 +3,14 @@ document.getElementById('form-cadastro').addEventListener('submit', async (event
     
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+    const password = document.getElementById('senha').value;
 
     const response = await fetch('https://pi-2sem.onrender.com/api/user', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ nome, email, senha })
+        body: JSON.stringify({ nome, email, password })
     });
     
     if (response.ok) {
